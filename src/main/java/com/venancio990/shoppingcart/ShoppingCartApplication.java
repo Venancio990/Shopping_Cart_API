9fdb16501501efbcd87d2fc9200d960b243e04bf
Venancio990 <venancio.123789@outlook.com>
2025-03-13 10:22:41 -0300

    Commiting and Pushing com.venancio990.shoppingcart

--- src/main/java/com/venancio990/shoppingcart/ShoppingCartApplication.java
+++ src/main/java/com/venancio990/shoppingcart/ShoppingCartApplication.java
@@ -1,4 +1,4 @@
-package com.example.shoppingcart;
+package com.venancio990.shoppingcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/venancio990/shoppingcart/ShoppingCartApplicationTests.java
+++ src/test/java/com/venancio990/shoppingcart/ShoppingCartApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.shoppingcart;
+package com.venancio990.shoppingcart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
